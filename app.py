440523737366f1bf48e0872548d33a2c5a812063
ZekiKarakomur <19145503@brookes.ac.uk>
2024-10-19 19:37:08 +0100

    linked flask to index page

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ app.config['DATABASE'] = 'database.sqlite'
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
